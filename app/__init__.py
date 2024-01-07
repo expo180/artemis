a30b2a33eeb278312e41afbd2677569f1c6dbd94
expo180 <badersaadousalissou@gmail.com>
2024-01-07 14:42:42 +0100

    switched to live

--- app/__init__.py
+++ app/__init__.py
@@ -18,14 +18,14 @@ db = SQLAlchemy()
 moment = Moment()
 migrate = Migrate()
 
-def create_app(development=True):
+def create_app(production=True):
     app = Flask(__name__)
     CORS(app)  # Enable CORS for all routes
 
-    app.config.from_object(config['development'])
+    app.config.from_object(config['production'])
     app.config['LANGUAGES'] = ['en', 'fr', 'ja', 'ar', 'it', 'es', 'pt', 'ru', 'pl']
     app.config['BABEL_DEFAULT_LOCALE'] = 'en'
-    config['development'].init_app(app)
+    config['production'].init_app(app)
     bootstrap.init_app(app)
     mail.init_app(app)
     db.init_app(app)
